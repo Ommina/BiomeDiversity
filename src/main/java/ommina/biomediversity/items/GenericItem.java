7b2974c7fd5e77debaedb9de4fbfbdf9df057569
ommina <lloyd@shaw.ca>
2019-08-12 04:13:38 -0600

    Add Orinocite ingot and cooking recipe.

---
+++ src/main/java/ommina/biomediversity/items/GenericItem.java
@@ -0,0 +1,15 @@
+package ommina.biomediversity.items;
+
+import net.minecraft.item.Item;
+import ommina.biomediversity.BiomeDiversity;
+
+public class GenericItem extends Item {
+
+    public GenericItem( String name, Item.Properties properties ) {
+        super( properties.group( BiomeDiversity.TAB ) );
+
+        setRegistryName( name );
+
+    }
+
+}
--- src/main/java/ommina/biomediversity/items/ModItems.java
+++ src/main/java/ommina/biomediversity/items/ModItems.java
@@ -8,16 +8,28 @@ import ommina.biomediversity.blocks.blocks.GenericBlock;
 public class ModItems {
 
     public static Item oreOrinocite;
+    public static Item ingotOrinocite;
 
     public static void register( final RegistryEvent.Register<Item> event ) {
 
         oreOrinocite = registerItem( event, ModBlocks.oreOrinocite, new Item.Properties() );
+        ingotOrinocite = registerItem( event, "orinocite_ingot", new Item.Properties() );
 
     }
 
     private static Item registerItem( final RegistryEvent.Register<Item> event, final GenericBlock fromBlock, Item.Properties properties ) {
 
         final GenericBlockItem item = new GenericBlockItem( fromBlock, properties );
+
+        event.getRegistry().register( item );
+
+        return item;
+
+    }
+
+    private static Item registerItem( final RegistryEvent.Register<Item> event, final String name, Item.Properties properties ) {
+
+        final GenericItem item = new GenericItem( name, properties );
 
         event.getRegistry().register( item );
 
